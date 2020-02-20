f1e1a24b289bf1509918a774bdc718be6d46d474
Teune <teune2000@hotmail.com>
2020-02-20 21:25:25 +0100

    Refactor

--- my-app/src/components/characters.js
+++ my-app/src/components/characters.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const Characters = ({ characters }) => {
-    return (
-      <div>
-        <center><h1>Characters List</h1></center>
-        {characters.map((character) => (
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">{character.name}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">{character.species}</h6>
-              <img src={character.image} alt='none' />
+  return (
+    <div class="row">
+      <center>
+        <h1>Characters List</h1>
+      </center>
+      <div class="row">
+        {characters.map(character => (
+          <div class="col-md-4 mb-3">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">{character.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">
+                  {character.species}
+                </h6>
+                <img class="img-fluid" src={character.image} alt="none" />
+              </div>
             </div>
           </div>
         ))}
       </div>
-    )
-  };
+    </div>
+  );
+};
 
-  export default Characters
+export default Characters;
