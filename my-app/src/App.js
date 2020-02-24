d5917c6122e395c5d19ace9b0b10397f4a111760
Tony <t.vanschaik@wearebert.com>
2020-02-24 09:54:13 +0100

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,4 +23,26 @@ class App extends Component {
     )  }
 }
 
+// class App extends Component {
+  
+//   state = {
+//     characters:[]
+//   }
+
+//   componentDidMount() {
+//     fetch('https://rickandmortyapi.com/api/character/')
+//     .then(res => res.json())
+//     .then((data) => {
+//       this.setState({ characters: data.results })
+//     })
+//     .catch(console.log)
+//   }
+  
+//   render() {
+//     return (
+//       <Characters characters={this.state.characters} />
+
+//     )  }
+// }
+
 export default App;
